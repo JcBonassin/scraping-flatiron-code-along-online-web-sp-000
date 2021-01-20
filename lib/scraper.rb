a6cac98af477fb0a7734c7ef7b4d3f6395a218c6
JC <bonassin@gmail.com>
2021-01-20 17:41:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-
+ 
 require_relative './course.rb'
-
+ 
 class Scraper
-
+ 
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
+ 
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+ 
     #doc.css(".post").each do |post|
       #course = Course.new
       #course.title = post.css("h2").text
@@ -16,6 +17,7 @@ class Scraper
       #course.description = post.css("p").text
     #end
   end
+ 
 end
 
   Scraper.new.get_page
